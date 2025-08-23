877950514235fd08142e56ecc3ee14ac9c82d33c
Songtaeha <songtaeha@naver.com>
2025-08-23 19:33:54 +0900

    feat(footer): 기본 푸터 컴포넌트 추가

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <div className="footer-content">
+        <p>&copy; 2025 Darcana. All rights reserved.</p>
+        <div className="footer-links">
+          <a href="#">Privacy</a>
+          <a href="#">Terms</a>
+          <a href="#">Contact</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
