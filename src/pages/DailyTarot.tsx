6f8f0887e83b600eb52d41a07b438a573f1ee755
Songtaeha <songtaeha@naver.com>
2025-08-25 18:31:20 +0900

    feat(dailytarot): DailyTarot 페이지 UI 구현

--- src/pages/DailyTarot.tsx
+++ src/pages/DailyTarot.tsx
@@ -1,5 +1,22 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import TarotCards from "../components/TarotCards";
+import TarotGirl from "../components/TarotGirl";
+import SpeechBubble from "../components/SpeechBubble";
+
 const DailyTarot = () => {
-  return <div>DailyTarot</div>;
+  return (
+    <div className="DailyTarot">
+      <Header />
+      <TarotCards />
+      <TarotGirl tarotgirlId={2} />
+      <SpeechBubble
+        bubbleId={2}
+        text={"카드 뽑기 버튼을 눌러 오늘의 카드를 뽑아보세요"}
+      />
+      <Footer />
+    </div>
+  );
 };
 
 export default DailyTarot;
