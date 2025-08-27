529e1115f14bbe74826dc12be2740d8dbd558c86
Songtaeha <songtaeha@naver.com>
2025-08-27 18:06:36 +0900

    feat(dailytarot):카드 뽑기시 이미지 변경 기능 추가

--- src/components/TarotCards.tsx
+++ src/components/TarotCards.tsx
@@ -1,16 +1,29 @@
 import "./TarotCards.css";
 import Button from "./Button";
+import cardBack from "../assets/CardBehind.png";
+import Card from "./Card";
 
 type TarotCardsProps = {
-  onClickBtn: () => void; // ✅ props 타입 정의
+  onClickBtn: () => void;
+  cardId?: number;
+  isReversed?: boolean;
 };
 
-const TarotCards = ({ onClickBtn }: TarotCardsProps) => {
+const TarotCards = ({ onClickBtn, cardId, isReversed }: TarotCardsProps) => {
+  const hasDraw = typeof cardId === "number";
+
   return (
     <div className="TarotCards">
       <div className="cards-container">
-        <div className="card"></div>
-        <Button text={"카드 뽑기"} onClick={onClickBtn} />
+        <div className="card">
+          {hasDraw ? (
+            <Card cardId={cardId!} isReversed={isReversed} />
+          ) : (
+            <img src={cardBack} alt="카드 뒷면" />
+          )}
+        </div>
+
+        <Button text="카드 뽑기" onClick={onClickBtn} />
       </div>
     </div>
   );
--- src/pages/DailyTarot.tsx
+++ src/pages/DailyTarot.tsx
@@ -20,15 +20,19 @@ const DailyTarot = () => {
   return (
     <div className="DailyTarot">
       <Header />
-      <TarotCards onClickBtn={onClickBtn} />
+      <TarotCards
+        onClickBtn={onClickBtn}
+        cardId={draw?.card.id}
+        isReversed={draw?.isReversed}
+      />
       <TarotGirl tarotgirlId={2} />
       <SpeechBubble
         bubbleId={2}
         text={
           draw
-            ? `${draw.card.id - 1}번 카드인 ${
-                draw.card.nameKo
-              } 카드를 뽑으셨습니다`
+            ? `${draw.card.id}번 카드인 ${draw.card.nameKo} 카드를 ${
+                draw.isReversed ? "역방향" : "정방향"
+              }으로 뽑으셨습니다`
             : "카드 뽑기 버튼을 눌러 오늘의 카드를 뽑아보세요"
         }
       >
