57ed8b7c25f81a108b15787974e3dedf0c117ae6
Songtaeha <songtaeha@naver.com>
2025-08-28 20:24:05 +0900

    style(tartotexplanation) : 결과 페이지 스타일 변경

--- src/components/TarotExplanation.tsx
+++ src/components/TarotExplanation.tsx
@@ -11,15 +11,50 @@ const TarotExplanation = () => {
   const isReversed = direction === "reversed";
   const cardData: TarotCard = majorArcana.find((c) => c.id === cardId)!;
 
+  const keywords = isReversed
+    ? cardData.keywordsReversed
+    : cardData.keywordsUpright;
+
+  const description = isReversed ? cardData.descReversed : cardData.descUpright;
+  const fortune = isReversed
+    ? cardData.fortuneReversed
+    : cardData.fortuneUpright;
+
   return (
     <div className="TarotExplanation">
       <div className="background"></div>
       <Card cardId={cardId} isReversed={isReversed} />
       <SpeechBubble
-        text={`${cardData.id}번 카드인 ${cardData.nameKo} 카드를 ${
-          isReversed ? "역방향" : "정방향"
-        }으로 뽑으셨습니다`}
         bubbleId={3}
+        text={
+          <>
+            <p className="title">
+              {`${cardData.id}번 카드인 ${cardData.nameKo} 카드를 ${
+                isReversed ? "역방향" : "정방향"
+              }으로 뽑으셨습니다.`}
+            </p>
+            <br />
+            {`${isReversed ? "역방향" : "정방향"} ${cardData.nameKo} 카드의 `}
+            <span className="highlight-label">키워드</span>
+            {`는 "`}
+            <span className="highlight-keyword">
+              {Array.isArray(keywords) ? keywords.join(", ") : keywords}
+            </span>
+            {` " 이며,`}
+            <br />
+            {`이는 "`}
+            <span className="highlight-desc">
+              {Array.isArray(description)
+                ? description.join(", ")
+                : description}
+            </span>
+            {`" 라는 의미를 지닙니다.`}
+
+            <br />
+            <br />
+            <span className="fortune-box">{fortune}</span>
+          </>
+        }
       />
     </div>
   );
