76d794c4e27d605ee8285ded7717ffee0c376f46
Songtaeha <songtaeha@naver.com>
2025-09-02 20:37:11 +0900

    feat(button) : active상태 추가 + active상태 스타일 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,14 @@
 import "./Button.css";
+
 type ButtonProps = {
   text: string;
   onClick: () => void;
+  isActive?: boolean;
 };
-const Button = ({ text, onClick }: ButtonProps) => {
+
+const Button = ({ text, onClick, isActive }: ButtonProps) => {
   return (
-    <button onClick={onClick} className="Button">
+    <button onClick={onClick} className={`Button ${isActive ? "active" : ""}`}>
       {text}
     </button>
   );
