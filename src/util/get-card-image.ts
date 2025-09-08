80856a93df148fae23d7e128486c5747cf013cea
Songtaeha <songtaeha@naver.com>
2025-09-08 20:30:40 +0900

    feat(minorarcana):마이너 카드 데이터추가 + 마이너 카드 이미지 가져오기

--- src/util/get-card-image.ts
+++ src/util/get-card-image.ts
@@ -20,6 +20,62 @@ import card18 from "../assets/cards/major/18.png";
 import card19 from "../assets/cards/major/19.png";
 import card20 from "../assets/cards/major/20.png";
 import card21 from "../assets/cards/major/21.png";
+import card22 from "../assets/cards/minor/22.png";
+import card23 from "../assets/cards/minor/23.png";
+import card24 from "../assets/cards/minor/24.png";
+import card25 from "../assets/cards/minor/25.png";
+import card26 from "../assets/cards/minor/26.png";
+import card27 from "../assets/cards/minor/27.png";
+import card28 from "../assets/cards/minor/28.png";
+import card29 from "../assets/cards/minor/29.png";
+import card30 from "../assets/cards/minor/30.png";
+import card31 from "../assets/cards/minor/31.png";
+import card32 from "../assets/cards/minor/32.png";
+import card33 from "../assets/cards/minor/33.png";
+import card34 from "../assets/cards/minor/34.png";
+import card35 from "../assets/cards/minor/35.png";
+import card36 from "../assets/cards/minor/36.png";
+import card37 from "../assets/cards/minor/37.png";
+import card38 from "../assets/cards/minor/38.png";
+import card39 from "../assets/cards/minor/39.png";
+import card40 from "../assets/cards/minor/40.png";
+import card41 from "../assets/cards/minor/41.png";
+import card42 from "../assets/cards/minor/42.png";
+import card43 from "../assets/cards/minor/43.png";
+import card44 from "../assets/cards/minor/44.png";
+import card45 from "../assets/cards/minor/45.png";
+import card46 from "../assets/cards/minor/46.png";
+import card47 from "../assets/cards/minor/47.png";
+import card48 from "../assets/cards/minor/48.png";
+import card49 from "../assets/cards/minor/49.png";
+import card50 from "../assets/cards/minor/50.png";
+import card51 from "../assets/cards/minor/51.png";
+import card52 from "../assets/cards/minor/52.png";
+import card53 from "../assets/cards/minor/53.png";
+import card54 from "../assets/cards/minor/54.png";
+import card55 from "../assets/cards/minor/55.png";
+import card56 from "../assets/cards/minor/56.png";
+import card57 from "../assets/cards/minor/57.png";
+import card58 from "../assets/cards/minor/58.png";
+import card59 from "../assets/cards/minor/59.png";
+import card60 from "../assets/cards/minor/60.png";
+import card61 from "../assets/cards/minor/61.png";
+import card62 from "../assets/cards/minor/62.png";
+import card63 from "../assets/cards/minor/63.png";
+import card64 from "../assets/cards/minor/64.png";
+import card65 from "../assets/cards/minor/65.png";
+import card66 from "../assets/cards/minor/66.png";
+import card67 from "../assets/cards/minor/67.png";
+import card68 from "../assets/cards/minor/68.png";
+import card69 from "../assets/cards/minor/69.png";
+import card70 from "../assets/cards/minor/70.png";
+import card71 from "../assets/cards/minor/71.png";
+import card72 from "../assets/cards/minor/72.png";
+import card73 from "../assets/cards/minor/73.png";
+import card74 from "../assets/cards/minor/74.png";
+import card75 from "../assets/cards/minor/75.png";
+import card76 from "../assets/cards/minor/76.png";
+import card77 from "../assets/cards/minor/77.png";
 
 const cards = [
   card00,
@@ -44,6 +100,62 @@ const cards = [
   card19,
   card20,
   card21,
+  card22,
+  card23,
+  card24,
+  card25,
+  card26,
+  card27,
+  card28,
+  card29,
+  card30,
+  card31,
+  card32,
+  card33,
+  card34,
+  card35,
+  card36,
+  card37,
+  card38,
+  card39,
+  card40,
+  card41,
+  card42,
+  card43,
+  card44,
+  card45,
+  card46,
+  card47,
+  card48,
+  card49,
+  card50,
+  card51,
+  card52,
+  card53,
+  card54,
+  card55,
+  card56,
+  card57,
+  card58,
+  card59,
+  card60,
+  card61,
+  card62,
+  card63,
+  card64,
+  card65,
+  card66,
+  card67,
+  card68,
+  card69,
+  card70,
+  card71,
+  card72,
+  card73,
+  card74,
+  card75,
+  card76,
+  card77,
 ];
 
 export function getCardImage(id: number) {
